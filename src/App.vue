fd5226dda3c785d35fb224bdc5d31997226c417c
Masaki-Ohara <masakingu921@gmail.com>
2023-09-22 23:07:41 +0900

    checkbox作成

--- src/App.vue
+++ src/App.vue
@@ -14,14 +14,57 @@
    </keep-alive>
    <div>
      <h2>イベントのフォーム</h2>
-     <label for="title">タイトル</label>
+    <label for="title">タイトル</label>
      <input 
        id="title"
        type="text"
-       v-model="eventData.title"
+       v-model.lazy="eventData.title"
        >
-        <P>{{eventData.title}}</P>
-   </div>
+        <Pre>{{eventData.title}}</Pre>
+     <label for="maxNumber">最大人数</label>
+     <input 
+       id="maxNUmber"
+       type="number"
+       v-model.number="eventData.maxNumber"
+       >
+        <Pre>{{typeof eventData.maxNumber}}</Pre>
+
+        <label for="host">主催者</label>
+     <input 
+       id="host"
+       type="text"
+       v-model.trim="eventData.host"
+       >
+        <Pre>{{eventData.host}}</Pre>
+        <textarea id="detail" cols="30" rows="10" v-model="eventData.detail"></textarea>
+        <pre>{{eventData.detail}}</pre>
+        <input type="checkbox" id="isPrivate" v-model="eventData.isPrivate">
+        <label for="eventData.isPrivate">非公開</label>
+        <p>{{eventData.isPrivate}}</p>
+        <p>参加条件</p>
+        <input
+          type="checkbox"
+          id="10"
+          value="10代"
+          v-model="eventData.target"
+        >
+        <label for="10">10代</label>
+                <input
+          type="checkbox"
+          id="20"
+          value="20代"
+          v-model="eventData.target"
+        >
+        <label for="20">20代</label>
+        <input
+          type="checkbox"
+          id="30"
+          value="30代"
+          v-model="eventData.target"
+        >
+        <label for="30">30代</label>
+        <p>{{eventData.target}}</p>
+    </div>
   </div>
 </template>
 
@@ -31,13 +74,19 @@ import About from "./components/About.vue";
 import Home from "./components/Home.vue";
 
 
+
 export default {
   data() {
     return {
       number: 10,
       currentComponent: "Home",
       eventData: {
-        title: "タイトル"
+        title: "タイトル",
+        maxNUmber: "0",
+        host: "",
+        detail: "",
+        isPrivate: false,
+        target: []
       }
     };
   },
