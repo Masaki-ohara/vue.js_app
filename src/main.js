e7745777b7a051ef822c18d884f8feffe0ea12f1
Masaki-Ohara <masakingu921@gmail.com>
2023-10-01 14:19:12 +0900

    フイルタ関数の連結とmixinを作成

--- src/main.js
+++ src/main.js
@@ -4,9 +4,20 @@ import LikeNumber from  "./components/LikeNumber.vue";
 
 Vue.config.productionTip = false
 Vue.component('LikeNumber', LikeNumber);
-Vue.directive("border", function(el, binding) {
-  el.style.border = "solid black 2px";
-  el.style.borderWidth = binding.value
+Vue.filter("upperCase", function(value){
+  console.log("フィルタ")
+  return value.toUpperCase();
+});
+// Vue.directive("border", function(el, binding) {
+//   el.style.borderWidth = binding.value.width;
+  // el.style.borderColor = binding.value.color;
+  // el.style.borderStyle = binding.arg;
+  // if (binding.modifiers.round) {
+  //   el.style.borderRadius = "0.5rem";
+  // }
+  // if (binding.modifiers.shadow) {
+  //   el.style.boxShadow = "0.2px 5px rgba(0, 0, 0, 0.26)";
+  // }
   // bind(el, binding, vnode) {
   //   //ディレクィブが初めて対象の要素に紐付いた時
   // },
@@ -22,7 +33,13 @@ Vue.directive("border", function(el, binding) {
   // unbind() {
   //   // ディレクティブがひも付いてる要素から取り除かれた時
   // },
-});
+// });
+Vue.mixin({
+  created() {
+    console.log("global mixin")
+  }
+})
+
 new Vue({
   render: h => h(App),
 }).$mount('#app')
